98a73618fe34381bb7e4ea95148581b16b702e2a
ruchit <tripathiruchit007@gmail.com>
2019-11-03 23:22:03 +0530

    Added Enhanced view for each post and added base template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import post
 from django.utils import timezone
+
+
 # Create your views here.
 
 def post_list(request):
     posts = post.objects.filter(title__contains="blog").order_by("published_date")
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    pos = get_object_or_404(post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'pos': pos})
